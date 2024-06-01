27dacbdf663cdc5de1a67c9e094a6c55e193b11d
Magnus Fernandes <magnusfernandes1295@hotmail.com>
2024-06-01 15:42:49 +0530

    Register form

--- src/views/auth/register.tsx
+++ src/views/auth/register.tsx
@@ -3,42 +3,82 @@ import { LoadingButton } from '@mui/lab';
 import { Button, Container, Grid, TextField, Typography } from '@mui/material';
 import { useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import * as yup from 'yup';
 
-interface FormSchemaType {
-  email: string;
-}
+import { makeRequest, RequestMethod } from '../../shared';
 
 const FormSchema = yup
   .object({
-    email: yup
+    name: yup.string().min(6),
+    phone: yup
       .string()
-      .email('Enter a valid email address')
-      .nullable()
-      .transform((curr, orig) => (orig === '' ? null : curr)),
+      .matches(/^[a-zA-Z0-9_]*/, 'This field contains only numbers')
+      .min(10),
+    email: yup.string().email(),
+    password: yup.string().min(6).required(),
+    cpassword: yup.string().min(6).required(),
   })
   .required();
 
 export const Register = () => {
+  const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
-  const [disableSubmit, setDisableSubmit] = useState(false);
   const {
     handleSubmit,
     control,
     setValue,
+    watch,
     formState: { errors, isDirty, isValid },
   } = useForm({
     mode: 'all',
     defaultValues: {
+      name: '',
       email: '',
+      phone: '',
+      password: '',
+      cpassword: '',
     },
     resolver: yupResolver(FormSchema),
   });
+  const watchPassword = watch('password');
+  const watchCpassword = watch('cpassword');
 
   const onSubmit = async (data: any) => {
-    setDisableSubmit(true);
     setLoading(true);
+    try {
+      const payload: any = {
+        name: data.name,
+        email: data.email,
+        phone: `+${data.phone}`,
+        password: data.password,
+      };
+
+      if (data.role > 0) {
+        payload['role'] = data.role;
+      }
+
+      const response = await makeRequest(
+        '/auth/signup',
+        RequestMethod.POST,
+        true,
+        payload
+      );
+      navigate('/auth/signin');
+    } catch (e) {
+      console.log(e);
+    }
+    setLoading(false);
+  };
+
+  const disableForm = (): boolean => {
+    if (!isValid || !isDirty || loading) {
+      return true;
+    }
+    if (watchPassword !== watchCpassword) {
+      return true;
+    }
+    return false;
   };
 
   return (
@@ -51,6 +91,28 @@ export const Register = () => {
           Sign up with us
         </Typography>
         <Grid container>
+          <Grid item sm={12}>
+            <Controller
+              name="name"
+              control={control}
+              render={({ field: { ref, onChange, onBlur, ...field } }) => (
+                <TextField
+                  {...field}
+                  ref={ref}
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  error={!!errors.name}
+                  fullWidth
+                  label="Full name"
+                  variant="outlined"
+                  InputLabelProps={{
+                    className: 'tw-text-slate-100',
+                  }}
+                  className="tw-mb-4"
+                />
+              )}
+            />
+          </Grid>
           <Grid item sm={12}>
             <Controller
               name="email"
@@ -69,25 +131,84 @@ export const Register = () => {
                   InputLabelProps={{
                     className: 'tw-text-slate-100',
                   }}
+                  className="tw-mb-4"
                 />
               )}
             />
           </Grid>
           <Grid item sm={12}>
-            <TextField
-              className="tw-my-8"
-              fullWidth
-              label="Password"
-              variant="outlined"
-              type="password"
-              InputLabelProps={{
-                className: 'tw-text-slate-100',
-              }}
+            <Controller
+              name="phone"
+              control={control}
+              render={({ field: { ref, onChange, onBlur, ...field } }) => (
+                <TextField
+                  {...field}
+                  ref={ref}
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  error={!!errors.phone}
+                  fullWidth
+                  label="Phone number (with country code)"
+                  variant="outlined"
+                  type="tel"
+                  InputLabelProps={{
+                    className: 'tw-text-slate-100',
+                  }}
+                  className="tw-mb-4"
+                />
+              )}
+            />
+          </Grid>
+
+          <Grid item sm={12}>
+            <Controller
+              name="password"
+              control={control}
+              render={({ field: { ref, onChange, onBlur, ...field } }) => (
+                <TextField
+                  {...field}
+                  ref={ref}
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  error={!!errors.password}
+                  fullWidth
+                  label="Password"
+                  variant="outlined"
+                  type="password"
+                  InputLabelProps={{
+                    className: 'tw-text-slate-100',
+                  }}
+                  className="tw-mb-4"
+                />
+              )}
+            />
+          </Grid>
+          <Grid item sm={12}>
+            <Controller
+              name="cpassword"
+              control={control}
+              render={({ field: { ref, onChange, onBlur, ...field } }) => (
+                <TextField
+                  {...field}
+                  ref={ref}
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  error={!!errors.cpassword}
+                  fullWidth
+                  label="Confirm password"
+                  variant="outlined"
+                  type="password"
+                  InputLabelProps={{
+                    className: 'tw-text-slate-100',
+                  }}
+                  className="tw-mb-4"
+                />
+              )}
             />
           </Grid>
           <Grid item sm={12}>
             <LoadingButton
-              disabled={!isValid || !isDirty || disableSubmit}
+              disabled={disableForm()}
               color="success"
               size="large"
               variant="contained"
